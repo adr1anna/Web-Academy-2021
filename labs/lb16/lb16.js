132eef41700b5e41b71a569e7b15c1df52e6050a
ADRIANNA SINGH <>
2022-05-17 16:52:22 +0000

    Initial commit

---
+++ labs/lb16/lb16.js
@@ -0,0 +1,25 @@
+function submitForum(){
+  var x = document.getElementById("before");
+  if (x.style.display ==="none"){
+    x.style.display === "block";
+  } else {
+    x.style.display="none"};
+}
+function submitForum() {
+  let userName=document.userform.input_username.value;
+  let userPhone=document.userform.input_phoneNum.value;
+  let userEmail=document.userform.input_email.value;
+  let userNameResult=document.getElementById("nameResult")
+  let userPhoneResult=document.getElementById("phoneNumResult")
+  let userEmailResult=document.getElementById("emailResult")
+
+  userNameResult.innerHTML=userName;
+  userPhoneResult.innerHTML=userPhone;
+  userEmailResult.innerHTML=userEmail;
+  console.log(userEmail,userPhone,userName);
+  document.getElementById("before").innerHTML="";
+}
+
+function returnHome(){
+  var x = document.getElementById
+}
